19d386005c4e8ab63802b7c737feaeae74740a93
gdnvyana <gdnvyanaa@gmail.com>
2026-04-13 03:08:04 +0300

    Homework: corrected my homework

--- automation-trainee/src/Homework.java
+++ automation-trainee/src/Homework.java
@@ -7,18 +7,20 @@ public class Homework {
         System.out.println();
 
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter your name: ");
         String userName = scanner.nextLine();
 
-        System.out.print(userName + ", please enter your score: ");
-        int score = scanner.nextInt();
-        scanner.nextLine();
-
-        while (score < 0 || score > 100) {
-            System.out.print(userName + ", incorrect value, please, enter your score again: ");
+        int score;
+        do {
+            System.out.print(userName + ", please enter your score (0-100): ");
             score = scanner.nextInt();
             scanner.nextLine();
-        }
+
+            if (score < 0 || score > 100) {
+                System.out.println(userName + ", incorrect value! Score must be between 0 and 100.");
+            }
+        } while (score < 0 || score > 100);
 
         if (score >= 90) {
             System.out.println(userName + ", your grade is A!");
@@ -30,7 +32,10 @@ public class Homework {
             System.out.println(userName + ", your grade is D!");
         } else if (score >= 50) {
             System.out.println(userName + ", your grade is E!");
-        } else System.out.println(userName + ", your grade is F!");
+        } else {
+            System.out.println(userName + ", your grade is F!");
+        }
+
 
         System.out.println("----------------------------------------------------------------------------");
         System.out.println("Homework number 2");
@@ -38,32 +43,27 @@ public class Homework {
 
         String userInput;
 
-        while (true) {
+        do {
             System.out.println(userName + ", please enter something or 'exit' for output:");
             userInput = scanner.nextLine();
 
             if (userInput.equalsIgnoreCase("exit")) {
                 System.out.println(userName + "Goodbye!");
-                break;
+            } else {
+                System.out.println("You entered: " + userInput);
+                System.out.println();
             }
-
-            System.out.println("You entered: " + userInput);
-            System.out.println();
-        }
+        } while (!userInput.equalsIgnoreCase("exit"));
         System.out.println("----------------------------------------------------------------------------");
         System.out.println("Homework number 3");
 
 
-        System.out.println(userName + ", please enter your number: ");
+        System.out.print(userName + ", please enter your number: ");
         int x = scanner.nextInt();
-        int sum;
 
-        if (x >= 0) {
-            sum = x * (x + 1) / 2;
-        } else {
-            sum = -((-x) * (-x + 1) / 2);
-        }
+        int sum = Math.abs(x) * (Math.abs(x) + 1) / 2;
+        int result = (x >= 0) ? sum : -sum;
 
-        System.out.printf("The sum of all numbers from 0 to %d is %d", x, sum);
+        System.out.printf("The sum of all numbers from 0 to %d is %d", x, result);
     }
 }
